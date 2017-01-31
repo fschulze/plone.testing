0bb94cb4c6300b5182a77ed7ef8bf0729c077315
David Glick <david@glicksoftware.com>
2017-01-31 12:21:03 +0100

    restore test browser (now using the WSGI publisher)

--- src/plone/testing/z2.py
+++ src/plone/testing/z2.py
@@ -5,6 +5,7 @@ from OFS.metaconfigure import get_packages_to_initialize
 from plone.testing import Layer
 from plone.testing import zca
 from plone.testing import zodb
+from plone.testing._z2_testbrowser import Browser  # noqa
 from Testing.ZopeTestCase.ZopeLite import _patched as ZOPETESTCASEALERT
 from zope.schema.vocabulary import getVocabularyRegistry
 from zope.schema.vocabulary import setVocabularyRegistry
